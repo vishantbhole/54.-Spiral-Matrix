98febf8fc33d3528f501c9f82539ec7ef4922a5e
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-07-28 21:12:33 -0500

    Update spiralOrder.py

--- spiralOrder.py
+++ spiralOrder.py
@@ -5,3 +5,11 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
+        res = []
+        if not matrix:
+            return res
+
+        top = 0
+        bottom = len(matrix) - 1
+        left = 0
+        right = len(matrix[0]) - 1

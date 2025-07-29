4bc107a7879081a1d5621ee8dd5a8f3f014b11f5
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-07-28 21:13:05 -0500

    Update spiralOrder.py

--- spiralOrder.py
+++ spiralOrder.py
@@ -13,3 +13,12 @@ class Solution(object):
         bottom = len(matrix) - 1
         left = 0
         right = len(matrix[0]) - 1
+        
+        while top <= bottom and left <= right:
+            for i in range(left, right + 1):
+                res.append(matrix[top][i])
+            top += 1
+
+            for i in range(top, bottom + 1):
+                res.append(matrix[i][right])
+            right -= 1

5ab4f83e3934100514a39ad76132e452082ba360
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-07-28 21:13:39 -0500

    Update spiralOrder.py

--- spiralOrder.py
+++ spiralOrder.py
@@ -22,3 +22,14 @@ class Solution(object):
             for i in range(top, bottom + 1):
                 res.append(matrix[i][right])
             right -= 1
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    res.append(matrix[bottom][i])
+                bottom -= 1
+
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    res.append(matrix[i][left])
+                left += 1
+
+        return res
